8d99bb4bd30cc4afa457eba2a943a942790ac926
Naoya Yamashita <conao3@gmail.com>
2022-10-01 22:00:12 +0900

    depends getopts and implement simple repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+extern crate getopts;
+
+
+fn repl() {
+    loop {
+        print!("> ");
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        println!("{}", input);
+    }
+}
+
+
+fn print_usage(program: &str, opts: getopts::Options) {
+    let brief = format!("Usage: {} [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let args: Vec<String> = std::env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = getopts::Options::new();
+    opts.optflag("h", "help", "print this help menu");
+    opts.optflag("v", "verbose", "print extra information");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!("{}", f.to_string()) }
+    };
+
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+
+    repl()
 }
