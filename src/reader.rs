189fca2c7714e43876b2714d736b7b3daacd2d8b
Naoya Yamashita <conao3@gmail.com>
2022-10-04 18:36:17 +0900

    split read_number function

--- src/reader.rs
+++ src/reader.rs
@@ -15,6 +15,30 @@ impl Reader<'_> {
         self.input = self.input.trim_start();
     }
 
+    fn read_number(&mut self) -> anyhow::Result<types::RuspExp> {
+        let mut inx = 0;
+        let mut is_float = false;
+
+        for c in self.input.chars() {
+            if c == '.' {
+                is_float = true;
+            }
+            inx += 1;
+        }
+
+        if is_float {
+            let f = self.input[..inx]
+                .parse::<f64>()
+                .with_context(|| "Failed to parse float")?;
+            return Ok(types::RuspExp::Atom(types::RuspAtom::Float(f)));
+        }
+
+        let i = self.input[..inx]
+            .parse::<i64>()
+            .with_context(|| "Failed to parse int")?;
+        Ok(types::RuspExp::Atom(types::RuspAtom::Int(i)))
+    }
+
     fn read_atom(&mut self) -> anyhow::Result<types::RuspExp> {
         let c = self
             .input
@@ -22,27 +46,7 @@ impl Reader<'_> {
             .next()
             .ok_or(types::RuspErr::ReaderEofError)?;
         match c {
-            '0'..='9' => {
-                let mut inx = 0;
-                let mut is_float = false;
-                for c in self.input.chars() {
-                    if c == '.' {
-                        is_float = true;
-                    }
-                    inx += 1;
-                }
-                if is_float {
-                    let f = self.input[..inx]
-                        .parse::<f64>()
-                        .with_context(|| "Failed to parse float")?;
-                    Ok(types::RuspExp::Atom(types::RuspAtom::Float(f)))
-                } else {
-                    let i = self.input[..inx]
-                        .parse::<i64>()
-                        .with_context(|| "Failed to parse int")?;
-                    Ok(types::RuspExp::Atom(types::RuspAtom::Int(i)))
-                }
-            }
+            '0'..='9' => self.read_number(),
             _ => Ok(types::RuspExp::Atom(types::RuspAtom::Symbol(
                 self.input.to_string(),
             ))),
