e9f96e0f575d0fbc47ee96b17fa2a892f8491fae
Naoya Yamashita <conao3@gmail.com>
2022-10-05 22:59:22 +0900

    implement minus function

--- src/builtin.rs
+++ src/builtin.rs
@@ -40,7 +40,27 @@ pub fn minus(arg: types::RuspExp) -> anyhow::Result<types::RuspExp> {
     let mut acc_float: f64 = 0.0;
     let mut float_p = false;
 
-    for exp_ in arg.into_iter() {
+    let mut iter = arg.into_iter();
+    let first = iter.next();
+    if first.is_none() {
+        return Ok(types::RuspExp::Atom(types::RuspAtom::Int(0)));
+    }
+
+    match first.unwrap() {
+        Ok(exp) => match **exp {
+            types::RuspExp::Atom(types::RuspAtom::Int(i)) => {
+                acc_int = i;
+            }
+            types::RuspExp::Atom(types::RuspAtom::Float(f)) => {
+                acc_float = f;
+                float_p = true;
+            }
+            _ => anyhow::bail!(types::RuspErr::WrongTypeArgument),
+        },
+        Err(e) => anyhow::bail!(e),
+    }
+
+    for exp_ in iter {
         let exp = exp_.with_context(|| types::RuspErr::WrongTypeArgument)?;
         match **exp {
             types::RuspExp::Atom(types::RuspAtom::Int(i)) => {
