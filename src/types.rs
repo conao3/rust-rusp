27b86cf63d7aed468c7b59b8d07917369f87f7e9
Naoya Yamashita <conao3@gmail.com>
2022-10-05 14:52:22 +0900

    commit

--- src/types.rs
+++ src/types.rs
@@ -8,30 +8,39 @@ pub enum RuspErr {
     ReaderEofError,
     #[error("ReaderInternalError")]
     ReaderInternalError,
+    #[error("TypeError")]
+    TypeError,
 }
 
-#[derive(Debug, PartialEq)]
-pub enum RuspAtom {
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum RuspAtom<'a> {
     Int(i64),
-    Float(f64),
-    String(String),
-    Symbol(String),
+    String(&'a str),
+    Symbol(&'a str),
 }
 
-#[derive(Debug, PartialEq)]
-pub enum RuspExp {
-    Atom(RuspAtom),
+pub const T: RuspExp = RuspExp::Atom(RuspAtom::Symbol("t"));
+pub const NIL: RuspExp = RuspExp::Atom(RuspAtom::Symbol("nil"));
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum RuspExp<'a> {
+    Atom(RuspAtom<'a>),
     Cons {
-        car: Box<RuspExp>,
-        cdr: Box<RuspExp>,
+        car: &'a RuspExp<'a>,
+        cdr: &'a RuspExp<'a>,
     },
 }
 
-impl std::fmt::Display for RuspAtom {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+#[derive(Debug, PartialEq)]
+pub struct RuspEnv<'a> {
+    pub bindings: std::collections::HashMap<String, RuspExp<'a>>,
+}
+
+impl std::fmt::Display for RuspAtom<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         let str = match self {
             RuspAtom::Int(i) => i.to_string(),
-            RuspAtom::Float(i) => i.to_string(),
+            // RuspAtom::Float(i) => i.to_string(),
             RuspAtom::String(s) => s.to_string(),
             RuspAtom::Symbol(s) => s.to_string(),
         };
@@ -39,36 +48,42 @@ impl std::fmt::Display for RuspAtom {
     }
 }
 
-impl std::fmt::Display for RuspExp {
+impl std::fmt::Display for RuspExp<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         let str = match self {
             RuspExp::Atom(atom) => atom.to_string(),
             RuspExp::Cons { car, cdr } => {
                 let mut lst: Vec<String> = vec![];
-                let mut cell = cdr;
+                let mut cell = *cdr;
 
                 lst.push(car.to_string());
                 while let RuspExp::Cons {
                     car: cell_car,
                     cdr: cell_cdr,
-                } = &**cell
+                } = *cell
                 {
                     lst.push(cell_car.to_string());
                     cell = cell_cdr;
                 }
 
-                if let RuspExp::Atom(atom) = &**cell {
-                    match atom {
-                        RuspAtom::Symbol(s) if s == "nil" => (),
-                        _ => {
-                            lst.push('.'.to_string());
-                            lst.push(atom.to_string());
-                        }
+                let cdr_atom = cell;
+                match *cdr_atom {
+                    NIL => (),
+                    _ => {
+                        lst.push('.'.to_string());
+                        lst.push(cdr_atom.to_string());
                     }
                 }
+
                 format!("({})", lst.join(" "))
             }
         };
         write!(f, "{}", str)
+    }
+}
+
+impl<'a> RuspExp<'a> {
+    pub fn cons(&'a self, car: &'a RuspExp<'a>) -> RuspExp<'a> {
+        RuspExp::Cons { car, cdr: self }
     }
 }
