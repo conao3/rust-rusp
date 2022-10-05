acba313a101f53daa720ab90d448097f09e31e07
Naoya Yamashita <conao3@gmail.com>
2022-10-05 11:44:40 +0900

    implement fmt::Display for RuspAtom and RuspExp

--- src/core.rs
+++ src/core.rs
@@ -1,18 +1,20 @@
+use crate::reader;
 use crate::types;
 
-fn read(x: &str) -> anyhow::Result<&str> {
+fn read(x: &str) -> anyhow::Result<types::RuspExp> {
+    let mut reader = reader::Reader::new(x);
+    reader.read()
+}
+
+fn eval(x: types::RuspExp) -> anyhow::Result<types::RuspExp> {
     Ok(x)
 }
 
-fn eval(x: &str) -> anyhow::Result<&str> {
-    Ok(x)
+fn print(x: types::RuspExp) -> anyhow::Result<String> {
+    Ok(x.to_string())
 }
 
-fn print(x: &str) -> anyhow::Result<&str> {
-    Ok(x)
-}
-
-pub fn rep(x: &str) -> anyhow::Result<&str> {
+pub fn rep(x: &str) -> anyhow::Result<String> {
     anyhow::ensure!(!x.is_empty(), types::RuspErr::ReaderEofError);
 
     print(eval(read(x)?)?)
--- src/types.rs
+++ src/types.rs
@@ -24,3 +24,45 @@ pub enum RuspExp {
         cdr: Box<RuspExp>,
     },
 }
+
+impl std::fmt::Display for RuspAtom {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let str = match self {
+            RuspAtom::Int(i) => i.to_string(),
+            RuspAtom::Float(i) => i.to_string(),
+            RuspAtom::String(s) => s.to_string(),
+            RuspAtom::Symbol(s) => s.to_string(),
+        };
+        write!(f, "{}", str)
+    }
+}
+
+impl std::fmt::Display for RuspExp {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let str = match self {
+            RuspExp::Atom(atom) => atom.to_string(),
+            RuspExp::Cons { car, cdr } => {
+                let mut lst: Vec<String> = vec![];
+                let mut cell = cdr;
+
+                lst.push(car.to_string());
+                while let RuspExp::Cons { car: cell_car, cdr: cell_cdr } = &**cell {
+                    lst.push(cell_car.to_string());
+                    cell = cell_cdr;
+                }
+
+                if let RuspExp::Atom(atom) = &**cell {
+                    match atom {
+                        RuspAtom::Symbol(s) if s == "nil" => (),
+                        _ => {
+                            lst.push('.'.to_string());
+                            lst.push(atom.to_string());
+                        }
+                    }
+                }
+                format!("({})", lst.join(" "))
+            }
+        };
+        write!(f, "{}", str)
+    }
+}
